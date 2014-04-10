e155ba204c91972d877ca9091d1286fe54f84010
Ahlon Chen <ahlon2002@gmail.com>
2014-04-10 17:31:28 +0800

    change head to assets

--- application/views/head/common_head.php
+++
@@ -1,5 +0,0 @@
-<link rel="stylesheet" href="<?php echo PUB_ASSETS_PATH;?>/bootstrap/dist/css/bootstrap.min.css"/>
-<link rel="stylesheet" href="<?php echo PUB_ASSETS_PATH;?>/bootstrap/dist/css/bootstrap-theme.min.css">
-
-<script type="text/javascript" src="<?php echo PUB_ASSETS_PATH;?>jquery/dist/jquery.min.js"></script>
-<script type="text/javascript" src="<?php echo PUB_ASSETS_PATH;?>bootstrap/dist/js/bootstrap.min.js"></script>
